fd5bf555d15360011c679cb72bf0f8c867a8096a
Norrick <Norrick.mcgee@gmail.com>
2019-04-05 14:19:23 -0500

    Created py file and readme

---
+++ get_partner.py
@@ -0,0 +1,57 @@
+from env import user, password, host
+import pandas as pd
+
+def ask_for_name():
+
+    return input('\n  \nWhat Last Name would yout like to look up?\n')
+
+
+def ask_for_module():
+    print("\nwhich module are you needing partner for?")
+    print('(Python: Module4, Linear Reg: Module5, Classification: Module6)')
+    print('type "all" for all modules')
+    return input()
+
+
+url = f'mysql+pymysql://{user}:{password}@{host}'
+
+db_url = url + '/ada_students'
+
+def main():
+    print("let's check partner groups!")
+    #student name
+    sname = ask_for_name()
+    module = ask_for_module()
+    all_modules = False
+    if module.isdigit():
+        module = int(module)
+    else:
+        all_modules =  (module.lower().strip() == 'all')
+
+
+    df = pd.read_sql('SELECT * FROM student_groups JOIN students USING(student_id)', db_url)
+
+
+    ### Module is int
+    if all_modules:
+        sdf = df[df['last_name']==sname]
+        module_ids = list(sdf['module_id'])
+        for i in module_ids:
+            sdf = df[df['last_name']==sname]
+            gdf = sdf[sdf['module_id']== i]
+            group_id = int(gdf.group_id)
+            group = df[df['group_id'] == group_id]
+            partners = group[group['module_id'] == i]
+            print(partners[['student_id','module_id','first_name','last_name']])
+    else:
+        sdf = df[df['last_name']==sname]
+        gdf = sdf[sdf['module_id']== module]
+        group_id = int(gdf.group_id)
+        group = df[df['group_id'] == group_id]
+        partners = group[group['module_id'] == module]
+        print(partners[['student_id','first_name','last_name',]])
+
+
+
+if __name__ == '__main__':
+    main()
